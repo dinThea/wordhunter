78351715e1fd4efa1c3f64a5b2a32fe6ae9da927
dinThea <iagoelifa@gmail.com>
2018-05-13 18:38:30 -0300

    bugs

--- ui.c
+++ ui.c
@@ -164,7 +164,7 @@ int abrirMenuCli(){
                     fgets_(aux, 100, 0, 0);
                     // Caso o sistema seja windows, abre a funcao integrada com a biblioteca conio
                     #else                    
-                    fgests_(aux, 100, 0, 0);
+                    fgets_(aux, 100, 0, 0);
                     #endif
                     /* As funcoes padroes do stdio nao foram usadas por darem echo automaticamente
                         na entrada do teclado
